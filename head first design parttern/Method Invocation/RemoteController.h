e781ffb3ecd60d94095a7eb83c66c75dcb6b1ac2
Allen-C-Guan <54201792+Allen-C-Guan@users.noreply.github.com>
2021-08-07 23:57:15 +0800

    update

--- head first design parttern/Method Invocation/RemoteController.h
+++ head first design parttern/Method Invocation/RemoteController.h
@@ -30,7 +30,13 @@ public:
         openCommands[slot] = std::make_shared<NoCommand>();
         closeCommands[slot] = std::make_shared<NoCommand>();
     }
+    // 无论是开启，关闭，还是undo动作，command都致力于将自己变成莫得感情的调用机器，没有任何实际的工作内容
+    // 所有的实际行为都在reciever内执行了， 用户调用cmd，cmd调用reicever来执行真正的动作。
 
+    // 这样做的好处就在于cmd的通用性！而通用性的好处就在于用户和reciever之间，以及不同的reciever与reciever之间，
+    // 均是解耦的！！！
+
+    // 机制越傻瓜，则机制越通用，机制的通用性就要求其实际工作的对象的封装性要好，且严格按着机制来执行。
     void onButtonWasPushed(int slot)
     {
         openCommands[slot]->execute();
