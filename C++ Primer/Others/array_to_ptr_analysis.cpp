4392f6fa80e8678d05b5c7aa690ec8c4a4111aef
Allen-C-Guan <54201792+Allen-C-Guan@users.noreply.github.com>
2021-10-14 11:18:19 +0800

    Update array_to_ptr_analysis.cpp

--- C++ Primer/Others/array_to_ptr_analysis.cpp
+++ C++ Primer/Others/array_to_ptr_analysis.cpp
@@ -13,6 +13,43 @@ void ArrWithPtr(int *arr)
 {
     std::cout << sizeof(arr) / sizeof(arr[0])<< std::endl;
 }
+
+class Foo {
+public:
+    using Arr = int(&)[10];
+    // 数组指针的返回
+    int* getArrPtr()
+    {
+        return arr;
+    }
+    // 数组引用的返回
+    auto getArrRef() ->int(&)[10]
+    {
+        return arr;
+    }
+    // 使用using返回数组引用
+    Arr getArrRef2() {
+        return arr;
+    }
+
+private:
+    int arr[10] = {1,2,3,4};
+};
+void testReturnArr() {
+    int a = 2;
+    Foo f;
+    auto arr = f.getArrPtr();
+    std::cout << arr[1] << std::endl;
+    auto arrRef = f.getArrRef();
+    std::cout << arrRef[2] << std::endl;
+    arrRef[2] = 10;
+    std::cout << arrRef[2] << std::endl;
+
+    Foo::Arr arr2 = f.getArrRef2();
+    std::cout << arrRef[2] << std::endl;
+}
+
+
 int main() {
     int arr[10];
     ArrWithRef(arr);
